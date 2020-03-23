70f197b5107e0a25c7d718a49cad48ce39163ff7
Terry-zh <496971418@qq.com>
2020-03-23 21:57:09 +0800

    [add] un_bias

---
+++ statistics/main_unbias.py
@@ -0,0 +1,34 @@
+import random
+import matplotlib.pyplot as plt
+
+from stats.descriptive_stats import mean, variance
+
+
+def variance_bias(data):
+    """无偏性方差"""
+    n = len(data)
+    if n <= 1:
+        return None
+
+    mean_value = mean(data)
+    return sum((e - mean_value) ** 2 for e in data) / n
+
+
+def sample(num_of_samples, sample_sz, var):
+    data = []
+    # 遍历样本
+    for _ in range(num_of_samples):
+        # 从 0-1的均匀分布中抽取 sample_sz 的个体组成的样本,mean 计算样本均值
+        data.append(var([random.uniform(0.0, 1.0) for _ in range(sample_sz)]))
+    return data
+
+
+if __name__ == '__main__':
+    data1 = sample(1000, 40, variance_bias)
+    plt.hist(data1, bins="auto", rwidth=0.8)
+    # 样本方差均值 实验值
+    plt.axvline(x=mean(data1), c="000")
+    # 总体方差均值 (b-a)^2/12  0.0, 1.0 理论值
+    plt.axvline(x=1 / 12, c="red")
+    print("bias: ", mean(data1), 1/12)
+    plt.show()
