33e8c83018e2a7b77b1edb24df71f3c7579cdd30
Terry-zh <496971418@qq.com>
2020-03-28 22:43:34 +0800

    [add] var_est

--- statistics/main_interval_est.py
+++ statistics/main_interval_est.py
@@ -1,5 +1,5 @@
 from stats.descriptive_stats import mean, std, variance
-from stats.interval_est import mean_ci_est, var_ci_est
+from stats.interval_est import mean_ci_est, var_ci_est, mean_diff_ci_t_est, mean_diff_ci_z_est, var_ratio_ci_est
 
 if __name__ == '__main__':
     # 18 岁月收入数据
@@ -9,9 +9,20 @@ if __name__ == '__main__':
 
     # 点估计
     # 区间估计
-    print("18-->mean:", mean(salary_18))
-    print("18-->mean_ci_est:", mean_ci_est(salary_18, 0.05))
-    print("35-->mean:", mean(salary_35))
-    print("35-->mean_ci_est:", mean_ci_est(salary_35, 0.05))
-    print(std(salary_18), variance(salary_18), var_ci_est(salary_18, 0.05))
-    print(std(salary_35), variance(salary_35), var_ci_est(salary_35, 0.05))
+    # print("18-->mean:", mean(salary_18))
+    # print("18-->mean_ci_est:", mean_ci_est(salary_18, 0.05))
+    # print("35-->mean:", mean(salary_35))
+    # print("35-->mean_ci_est:", mean_ci_est(salary_35, 0.05))
+    # print(std(salary_18), variance(salary_18), var_ci_est(salary_18, 0.05))
+    # print(std(salary_35), variance(salary_35), var_ci_est(salary_35, 0.05))
+
+    # 两个总体方差未知且相等，求均值差的置信区间
+    # print(mean_diff_ci_t_est(salary_18, salary_35, 0.05, True))
+    # 两个总体方差未知且不等，求均值差的置信区间
+    # print(mean_diff_ci_t_est(salary_18, salary_35, 0.05, False))
+
+    # 两个总体方差已知，求均值差的置信区间 --- sigma->标准差
+    print(mean_diff_ci_z_est(salary_18, salary_35, 0.05, 1035, 1240))
+
+    # 两个总体均值未知，求方差比的置信区间
+    print(var_ratio_ci_est(salary_18, salary_35, 0.05))
--- statistics/stats/interval_est.py
+++ statistics/stats/interval_est.py
@@ -1,6 +1,6 @@
 from math import sqrt
 
-from scipy.stats import norm, t, chi2
+from scipy.stats import norm, t, chi2, f
 
 from stats.descriptive_stats import mean, std, variance
 
@@ -32,3 +32,61 @@ def var_ci_est(data, alpha):
     chi2_lower_value = chi2.ppf(alpha / 2, n - 1)
     chi2_upper_value = chi2.ppf(1 - alpha / 2, n - 1)
     return (n - 1) * s2 / chi2_upper_value, (n - 1) * s2 / chi2_lower_value
+
+
+def mean_diff_ci_t_est(data1, data2, alpha, equal=True):
+    # 样本容量 1
+    n1 = len(data1)
+    # 样本容量 2
+    n2 = len(data2)
+    # 均值差
+    mean_diff = mean(data1) - mean(data2)
+    # 样本方差
+    sample1_var = variance(data1)
+    sample2_var = variance(data2)
+
+    # 两个总体方差未知且相等，求均值差的置信区间
+    if equal:
+        # 联合标准差
+        sw = sqrt(((n1 - 1) * sample1_var + (n2 - 1) * sample2_var) / (n1 + n2 - 2))
+        t_value = abs(t.ppf(alpha / 2, n1 + n2 - 2))
+        return mean_diff - sw * sqrt(1 / n1 + 1 / n2) * t_value, \
+               mean_diff + sw * sqrt(1 / n1 + 1 / n2) * t_value
+
+    # 两个总体方差未知且不等，求均值差的置信区间
+    else:
+        # 自由度
+        # 分子
+        df_numerator = (sample1_var / n1 + sample2_var / n2) ** 2
+        # 分母
+        df_denominator = (sample1_var / n1) ** 2 / (n1 - 1) + (sample2_var / n2) ** 2 / (n2 - 1)
+        df = df_numerator / df_denominator
+        t_value = abs(t.ppf(alpha / 2, df))
+        return mean_diff - sqrt(sample1_var / n1 + sample2_var / n2) * t_value, \
+               mean_diff + sqrt(sample1_var / n1 + sample2_var / n2) * t_value
+
+
+def mean_diff_ci_z_est(data1, data2, alpha, sigma1, sigma2):
+    # 样本容量 1
+    n1 = len(data1)
+    # 样本容量 2
+    n2 = len(data2)
+    # 均值差
+    mean_diff = mean(data1) - mean(data2)
+    z_value = abs(norm.ppf(alpha / 2))
+    return mean_diff - sqrt(sigma1 ** 2 / n1 + sigma2 ** 2 / n2) * z_value, \
+           mean_diff + sqrt(sigma1 ** 2 / n1 + sigma2 ** 2 / n2) * z_value
+
+
+def var_ratio_ci_est(data1, data2, alpha):
+    # 样本容量 1
+    n1 = len(data1)
+    # 样本容量 2
+    n2 = len(data2)
+    # 置信下限
+    f_lower_value = f.ppf(alpha / 2, n1 - 1, n2 - 1)
+    # 置信上限
+    f_upper_value = f.ppf(1 - alpha / 2, n1 - 1, n2 - 1)
+    # 方差比
+    var_ratio = variance(data1) / variance(data2)
+    return var_ratio / f_upper_value, var_ratio / f_lower_value
